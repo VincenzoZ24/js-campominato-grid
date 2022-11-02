f9e168ac71828362bf5d9aa06de131ab015b3aa2
VincenzoZ24 <zitov287@gmail.com>
2022-11-02 17:15:24 +0100

    cambio livello difficolta assegnato variabili

--- js/script.js
+++ js/script.js
@@ -1,4 +1,9 @@
 let container = document.querySelector(".container");
+let livello = document.querySelector(".livello");
+let play = document.querySelector(".play");
+let indiceSelezionato = livello.selectedIndex;
+let valoreSelezionato = livello.options[0];
+let valueOption = valoreSelezionato.value;
 
 for (let i = 1; i <= 100; i++) {
     let cell = document.createElement("div");
@@ -7,5 +12,23 @@ for (let i = 1; i <= 100; i++) {
     cell.innerHTML = i;
     cell.addEventListener("click", function () {
         cell.classList.toggle("active");
-    })
-}
+    });
+};
+
+
+
+
+// if (livello.options[1]) {
+    
+//     play.addEventListener("click", function () {
+//         for (let i = 1; i <= 81; i++) {
+//             let cell = document.createElement("div");
+//             cell.classList.add("cell_medium");
+//             container.append(cell);
+//             cell.innerHTML = i;
+//             cell.addEventListener("click", function () {
+//                 cell.classList.toggle("active");
+//             });
+//         };
+//     })
+// }
