b953b8b52e733510e4bf558253ba6eeb75204784
VincenzoZ24 <zitov287@gmail.com>
2022-11-02 14:58:27 +0100

    creata griglia e aggiunto evento click

--- js/script.js
+++ js/script.js
@@ -1,6 +1,11 @@
 let container = document.querySelector(".container");
-for (let i = 0; i < 100; i++) {
+
+for (let i = 1; i <= 100; i++) {
     let cell = document.createElement("div");
     cell.classList.add("cell");
     container.append(cell);
+    cell.innerHTML = i;
+    cell.addEventListener("click", function () {
+        cell.classList.toggle("active");
+    })
 }
