0176020b924742cebaaf8aa06872e40b51fe25ef
VincenzoZ24 <zitov287@gmail.com>
2022-11-02 18:39:13 +0100

    definiti i primi due livelli

--- js/script.js
+++ js/script.js
@@ -6,6 +6,20 @@ let indiceSelezionato = livello.selectedIndex;
 let valoreSelezionato = livello.options[0];
 let valueOption = valoreSelezionato.value;
 
+for (let i = 1; i <= 100; i++) {
+    let cell = document.createElement("div");
+    cell.classList.add("cell");
+    container.append(cell);
+    cell.innerHTML = i;
+    cell.addEventListener("click", function () {
+        cell.classList.toggle("active");
+    });
+};
+
+
+
+
+// container_medium.classList.add("display_none")
 // for (let i = 1; i <= 100; i++) {
 //     let cell = document.createElement("div");
 //     cell.classList.add("cell");
@@ -16,27 +30,46 @@ let valueOption = valoreSelezionato.value;
 //     });
 // };
 
+// if (livello.options[1]) {
 
-if  (livello.options[0]){
-    
-    container_medium.classList.add("display_none")
-    for (let i = 1; i <= 100; i++) {
-        let cell = document.createElement("div");
-        cell.classList.add("cell");
-        container.append(cell);
-        cell.innerHTML = i;
-        cell.addEventListener("click", function () {
-            cell.classList.toggle("active");
-        });
-    };
-}
+//     play.addEventListener("click", function () {
+//         container_medium.replaceChildren()
 
-if (livello.options[1]) {
-    
-    play.addEventListener("click", function () {
-        if(livello.options[1]){
-            container_medium.classList.remove("display_none")
-        container.classList.add("display_none")
+//         container_medium.classList.remove("display_none")
+//         container.classList.add("display_none")
+//         for (let i = 1; i <= 81; i++) {
+//             let cell_medium = document.createElement("div");
+//             cell_medium.classList.add("cell_medium");
+//             container_medium.append(cell_medium);
+//             cell_medium.innerHTML = i;
+//             cell_medium.addEventListener("click", function () {
+//                 cell_medium.classList.toggle("active");
+//             });
+//         };
+
+
+//     })
+// }
+
+play.addEventListener("click", function () {
+    container.replaceChildren()
+    if (livello.value === "Easy") {
+        container_medium.classList.add("display_none");
+        container.classList.remove("display_none");
+        for (let i = 1; i <= 100; i++) {
+            let cell = document.createElement("div");
+            cell.classList.add("cell");
+            container.append(cell);
+            cell.innerHTML = i;
+            cell.addEventListener("click", function () {
+                cell.classList.toggle("active");
+            });
+        };
+
+    } else if (livello.value === "Medium") {
+        container_medium.replaceChildren()
+        container_medium.classList.remove("display_none");
+        container.classList.add("display_none");
         for (let i = 1; i <= 81; i++) {
             let cell_medium = document.createElement("div");
             cell_medium.classList.add("cell_medium");
@@ -46,8 +79,5 @@ if (livello.options[1]) {
                 cell_medium.classList.toggle("active");
             });
         };
-        }
-        
-    })
-} 
-
+    }
+})
